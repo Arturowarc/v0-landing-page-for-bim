e3802f01683465d596c59e8c1fecadb478d50497
Arturo Moyano <63552436+Arturowarc@users.noreply.github.com>
2026-01-05 16:23:06 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,22 +17,9 @@ export const metadata: Metadata = {
   keywords: "BIM, Building Information Modeling, 3D modeling, BIM coordination, Engineering, construction, AEC, Architect",
   generator: "v0.app",
   icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
-  },
+    icon: "/icon.svg",
+    apple: "/icon.svg",    
+    },
 }
 
 export default function RootLayout({
